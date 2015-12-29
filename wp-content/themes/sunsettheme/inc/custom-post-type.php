115946230278c4f7ab99db5ca175e622b2e9115e
Vince <vince.campbell@gmail.com>
2015-12-29 15:26:13 -0500

    End of Lesson 11 update

--- wp-content/themes/sunsettheme/inc/custom-post-type.php
+++ wp-content/themes/sunsettheme/inc/custom-post-type.php
@@ -10,6 +10,8 @@
 $contact = get_option('contact_form');
 if(@$contact == 1){
 	add_action('init', 'sunset_contact_custom_post_type');
+	add_filter('manage_sunset-contact_posts_columns', 'sunset_set_contact_columns');
+	add_action('manage_sunset-contact_posts_custom_column', 'sunset_contact_custom_column', 10, 2);
 }
 
 /* Contact Custom Post Type */
@@ -31,4 +33,25 @@ function sunset_contact_custom_post_type(){
 		'supports'          => array('title', 'editor', 'author')
 	);
 	register_post_type('sunset-contact', $args);
+}
+
+function sunset_set_contact_columns(){
+	$newColumns = array();
+	$newColumns['title'] = 'Full Name';
+	$newColumns['message'] = 'Message';
+	$newColumns['email'] = 'Email';
+	$newColumns['date'] = 'Date';
+	return $newColumns;
+}
+
+function sunset_contact_custom_column($column){
+	switch($column){
+		case 'message':
+			echo get_the_excerpt();
+			break;
+		case 'email':
+			// email column
+			echo 'email@email.com';
+			break;
+	}
 }
